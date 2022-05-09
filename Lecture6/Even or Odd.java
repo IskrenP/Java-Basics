21941110706a869115781e6fb3721e555e28127b
IskrenP <100073655+IskrenP@users.noreply.github.com>
2022-05-09 17:04:12 +0300

    Lecture 6

---
+++ Lecture6/Even or Odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lekciq1
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextDouble();
+        if (num1 % 2 == 0){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
+    
