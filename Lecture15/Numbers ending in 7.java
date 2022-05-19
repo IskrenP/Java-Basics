ea7bed6655c50dc76b29a64e833d73d2d674e66a
IskrenP <100073655+IskrenP@users.noreply.github.com>
2022-05-19 09:31:48 +0300

    Lecture 15

---
+++ Lecture15/Numbers ending in 7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class LiveDemo {
+    public static void main(String[] args) {
+ 
+        for (int i = 7; i <= 1000; i += 10) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+        }
+    }
+}
